58d3b6bbc8eaab5199f602bc3f688e5df8017896
karthikishoresounder <karthikishoresounder@karthikndersMBP.lan>
2022-03-13 00:40:55 -0600

    new commit#2

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper' 
 
 RSpec.describe CommentsController, type: :controller do
-  user = FactoryBot.create(:user)   
-  post = user.posts.create(FactoryBot.attributes_for(:post))
-  comment = post.comments.create(user_id: user.id, description: FactoryBot.attributes_for(:comment))
+  before do 
+    @user = FactoryBot.create(:user)   
+    @post = user.posts.create(FactoryBot.attributes_for(:post))
+    @comment = post.comments.create(user_id: user.id, description: FactoryBot.attributes_for(:comment))
+  end
 end
