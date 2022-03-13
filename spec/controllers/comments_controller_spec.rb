715eaed3ca5e6ba4ee06eebbd19ffab55637b7ae
karthikishoresounder <karthikishoresounder@karthikndersMBP.lan>
2022-03-13 14:18:37 -0500

    tests fixed

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper' 
 
 RSpec.describe CommentsController, type: :controller do
-  before do 
-    @user = FactoryBot.create(:user)   
-    @post = user.posts.create(FactoryBot.attributes_for(:post))
-    @comment = post.comments.create(user_id: user.id, description: FactoryBot.attributes_for(:comment))
-  end
-
-  describe 'creates a comment' do
-    
-  end
 end
